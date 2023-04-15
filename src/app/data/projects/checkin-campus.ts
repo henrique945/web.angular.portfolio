7329af6ca282c1334d5894857bc7dae57694493c
Henrique <henrique.rodrigues7@hotmail.com>
2023-04-14 22:45:19 -0300

    update(checkinCampus): add tags and techs

--- src/app/data/projects/checkin-campus.ts
+++ src/app/data/projects/checkin-campus.ts
@@ -1,4 +1,6 @@
 import { OrientationEnum } from '../../models/enums/orientation.enum';
+import { ProjectTagsEnum } from '../../models/enums/project-tags.enum';
+import { TechEnum } from '../../models/enums/tech.enum';
 import { ProjectInterface } from '../../models/interfaces/project.interface';
 
 export const checkinCampus: ProjectInterface = {
@@ -10,7 +12,7 @@ export const checkinCampus: ProjectInterface = {
     'Project to control student access to <b>Facens (SP-Brazil)</b> and <b>Newton (MG-Brazil)</b> university campuses during the pandemic.\n\n' +
     'The student needs to answer some questions about his health to be released on campus. In case access is denied, the student is removed according to the severity of the symptoms.\n\n' +
     'In the <b>administrative panel, the administrator has access to graphs of students</b> who passed or were quarantined and their reasons.\n',
-  outcome: 'Due to being mandatory to enter the campus, the system had more than 10 thousand students and great simultaneous access, being necessary to scale the amount of lambdas in Aws.',
+  outcome: 'Due to being mandatory to enter the campus, the system had more than 10k students and great simultaneous access, being necessary to scale the amount of lambdas in Aws.',
   imageUrls: [
     'assets/imgs/projects/checkinCampus/checkinCampus1.png',
     'assets/imgs/projects/checkinCampus/checkinCampus2.png',
@@ -26,8 +28,8 @@ export const checkinCampus: ProjectInterface = {
   links: [
     { title: 'Site', link: 'https://checkin-campus.com/' },
   ],
-  tags: [],
-  techs: [],
+  tags: [ProjectTagsEnum.MOBILE, ProjectTagsEnum.WEB, ProjectTagsEnum.APIS],
+  techs: [TechEnum.IONIC, TechEnum.ANGULAR, TechEnum.PWA, TechEnum.NESTJS],
   orientation: OrientationEnum.VERTICAL,
   position: 3,
 };
